e0e7b6c290b774b987a5e7a0031d02547e7a3fdd
tiencuong07 <20020375@vnu.edu.vn>
2024-04-22 22:37:35 +0700

    add config db

---
+++ server/config/db.js
@@ -0,0 +1,22 @@
+const { Sequelize } = require("sequelize");
+
+const db = new Sequelize(
+  "perform_library",
+  "root",
+  "171ca9e4eb616be4a8007db9fa1da1091480f211e39c2ed8cc56cd6d6d25b09a",
+  {
+    host: "222.252.25.37",
+    dialect: "mysql",
+    port: 3307,
+    logging: false,
+  }
+);
+
+try {
+  db.authenticate();
+  console.log("Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}
+
+module.exports = db;
