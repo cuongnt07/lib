3ace93650c94d120d374efeb2270d8cce734dd37
tiencuong07 <20020375@vnu.edu.vn>
2024-05-08 20:54:02 +0700

    fix category

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -1,38 +1,38 @@
-const db = require('../models/index');
-const sequelize = require('sequelize');
+const db = require("../models/index");
+const sequelize = require("sequelize");
 
 class CategoryController {
-
-    async createNewCategory(req, res) {
-        try {
-            const category = req.body;
-            await db.category.create({
-                category_name: category.category_name,
-                description: category.description
-            })
-            return res.status(200).json({
-                errCode: 0,
-                msg: 'Create category successfully!'
-            })
-        } catch (err) {
-            console.log(err)
-            return res.status(500).json("error")
-        }
+  async createNewCategory(req, res) {
+    try {
+      const category = req.body;
+      await db.category.create({
+        category_name: category.category_name,
+        description: category.description,
+      });
+      return res.status(200).json({
+        errCode: 0,
+        msg: "Create category successfully!",
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json("error");
     }
+  }
 
-    async getAllCategory(req, res) {
-        try{
-            const result = await db.category.sequelize.query('SELECT * FROM categories', { type: sequelize.QueryTypes.SELECT });
-            return res.status(200).json({
-                errCode: 0,
-                msg: 'Get all category successfully!',
-                result
-            })
-        } catch(err) {
-            console.log(err)
-            return res.status(500).json("error")
-        }
+  async getAllCategory(req, res) {
+    try {
+      const data = await db.category.sequelize.query(
+        "SELECT * FROM categories",
+        { type: sequelize.QueryTypes.SELECT }
+      );
+      return res.status(200).json({
+        data,
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json("error");
     }
+  }
 }
 
-module.exports = new CategoryController;
+module.exports = new CategoryController();
