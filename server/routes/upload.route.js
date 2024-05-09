49bd8d5d99700eef87e1cbec177404efcb2b070e
tiencuong07 <20020375@vnu.edu.vn>
2024-05-10 01:04:46 +0700

    commit

--- server/routes/upload.route.js
+++ server/routes/upload.route.js
@@ -4,12 +4,25 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs')
 
+function convertToFileName(text) {
+    let baseName = path.basename(text, path.extname(text)); // Lấy tên file không bao gồm phần mở rộng
+    let fileName = baseName
+      .toLowerCase() // Chuyển đổi tất cả các ký tự sang chữ thường
+      .trim() // Xóa khoảng trắng đầu và cuối chuỗi
+      .normalize("NFD") // Chuyển đổi các ký tự dấu sang các ký tự không dấu
+      .replace(/[\u0300-\u036f]/g, "") // Loại bỏ các ký tự dấu
+      .replace(/đ/g, "d") // Chuyển chữ "đ" thành "d"
+      .replace(/[^a-zA-Z0-9]+/g, "-") // Thay thế các ký tự không phải chữ cái hoặc số bằng dấu "-"
+      .replace(/^-+|-+$/g, ""); // Xóa các dấu "-" ở đầu hoặc cuối chuỗi
+    return fileName + path.extname(text); // Thêm lại phần mở rộng của file
+}
+
 var storage = multer.diskStorage({
     destination: function(req, file, cb) {
         cb(null, path.join('uploads'))
     },
     filename: async function (req, file, cb) {
-        var name = file.originalname ;
+        var name = convertToFileName(file.originalname);
         cb(null, name);
     }
 })
