b1d14d2635de73f420b8a4db895801df9ce1bfc7
tiencuong07 <20020375@vnu.edu.vn>
2024-05-15 20:16:15 +0700

    commit

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -3,6 +3,6 @@ var router = express.Router();
 let LoginController = require('../controllers/userController')
 const authenticateMiddleware = require('../middlewares/authenticate')
 //
-router.post('/login', authenticateMiddleware.authenticate, LoginController.login);
+router.post('/login', LoginController.login);
 
 module.exports = router;
