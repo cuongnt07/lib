80529a25a1f94000d9e517c27ee1d63c258a3984
tiencuong07 <20020375@vnu.edu.vn>
2024-05-23 01:58:28 +0700

    fix checkStudent

--- server/routes/book.route.js
+++ server/routes/book.route.js
@@ -7,7 +7,7 @@ const authorize = require('../middlewares/authorize')
 router.post('/book', bookController.createNewBook);
 router.get('/book', bookController.getAllBook)
 router.put('/book/:id',bookController.updateBook)
-router.get('/book/search', authenticateMiddleware.authenticate, authorize.checkLibrarian, bookController.searchBook)
+router.get('/book/search', authenticateMiddleware.authenticate, authorize.checkStudent, bookController.searchBook)
 router.get('/book/filter', bookController.filterBookByCategory)
 router.get('/book/:id', bookController.getBookDetail)
 router.get('/book/amount-book/:id',bookController.countBookIdleByBookLineID)
