c4f777804602e8f34689ec0c1b36d8144ff83cde
tiencuong07 <20020375@vnu.edu.vn>
2024-05-24 02:45:33 +0700

    commit

--- server/routes/book.route.js
+++ server/routes/book.route.js
@@ -10,7 +10,7 @@ router.put('/book/:id', authenticateMiddleware.authenticate, bookController.upda
 router.get('/book/search', authenticateMiddleware.authenticate, authorize.checkStudent, bookController.searchBook)
 router.get('/book/filter', authenticateMiddleware.authenticate, authorize.checkStudent, bookController.filterBookByCategory)
 router.get('/book/:id', authenticateMiddleware.authenticate, bookController.getBookDetail)
-router.get('/book/amount-book/:id', authenticateMiddleware.authenticate, authorize.checkStudent, bookController.countBookIdleByBookLineID)
+router.get('/book/amount-book/:id', bookController.countBookIdleByBookLineID)
 
 router.get("/book-count", authenticateMiddleware.authenticate, authorize.checkLibrarian, bookController.getBookCount);
 router.get("/newBook-count/date", authenticateMiddleware.authenticate, authorize.checkLibrarian, bookController.getNumNewBooks);
