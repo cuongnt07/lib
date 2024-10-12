31cefb8ba4ae24a8de9210f936909eadf132671c
cuongnt07 <92966894+cuongnt07@users.noreply.github.com>
2024-10-12 15:47:05 +0700

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,11 @@
 const { Sequelize } = require("sequelize");
 
 const db = new Sequelize(
-  "perform_library",
+  "railway",
   "root",
-  "171ca9e4eb6168007db9fa1da1091480f211e39c2ed8cc56cd6d6d25b09a",
+  "uLWaYxjxdgEpZFdlBtcGJDMOqXnTXoKc",
   {
-    host: "222.252.24.27",
+    host: "mysql-production-bc34.up.railway.app",
     dialect: "mysql",
     port: 3307,
     logging: false,
